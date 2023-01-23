c562905c85ff14cf04fa51b78ac2201aa328a992
Beidysy <beydisy@gmail.com>
2023-01-23 15:08:17 -0500

    Adding the APIs.py

---
+++ APIs.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+response = requests.get("https://api.tomitokko.repl.co/")
+
+print(response.status_code)
+res = json.loads(response.text)
+
+# print(res)
+
+for data in res:
+    print(data)
